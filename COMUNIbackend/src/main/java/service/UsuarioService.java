2274fb0a14cbbfd3910d6f67b91afc89d2b08757
Joaquín Villagra Pacheco <villagra.pacheco.joaquin@gmail.com>
2015-12-12 20:18:43 -0300

    Agregado ServicioUsuario

---
+++ COMUNIbackend/src/main/java/service/UsuarioService.java
@@ -0,0 +1,63 @@
+package service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import facade.ActorFacade;
+import model.Actor;
+import model.Film;
+
+@Path("/usuarios")
+public class ActorService {
+	
+	@EJB 
+	UsuarioFacade usuarioEJB;
+	
+	Logger logger = Logger.getLogger(UsuarioService.class.getName());
+	
+	@GET
+	@Produces({"application/xml", "application/json"})
+	public List<Usuario> findAll(){
+		return usuarioEJB.findAll();
+	}
+	
+	@GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Actor find(@PathParam("id") Integer id) {
+        return usuarioEJB.find(id);
+    }
+	
+	@POST
+    @Consumes({"application/xml", "application/json"})
+    public void create(Usuario entity) {
+		usuarioEJB.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({"application/xml", "application/json"})
+    public void edit(@PathParam("id") Integer id, Usuario entity) {
+    	entity.setUserId(id.intValue());
+    	usuarioEJB.edit(entity);
+    }
+    
+    /*
+    @GET
+    @Path("{id}/films")
+    @Produces({"application/xml", "application/json"})
+    public List<Film> find_films(@PathParam("id") Integer id) {
+        return actorFacadeEJB.find(id).obtenerFilms();
+    }
+	*/
+
+}
